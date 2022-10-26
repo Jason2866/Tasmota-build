6435c1712ffd3715a1567ab5a165e6c481f2e966
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-26 22:45:43 +0200

    Update copy_safeboot.py

--- copy_safeboot.py
+++ copy_safeboot.py
@@ -1,25 +1,25 @@
 from urllib import request
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32-safeboot.bin"
-response = request.urlretrieve(URL)
-
+response = request.urlretrieve(URL, "tasmota32-safeboot.bin")
+t
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32solo1-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32solo1-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32c3-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32c3-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32c3cdc-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32c3cdc-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s2-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32s2-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s2cdc-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32s2cdc-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s3-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32s3-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s3cdc-safeboot.bin"
-response = request.urlretrieve(URL)
+response = request.urlretrieve(URL, "tasmota32s3cdc-safeboot.bin")
