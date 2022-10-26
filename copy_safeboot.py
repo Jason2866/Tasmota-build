eb2873ab5240dd3eaa3bc568c9c3a9e740d9a79c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-26 21:55:28 +0200

    Create copy_safeboot.py

---
+++ copy_safeboot.py
@@ -0,0 +1,8 @@
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32solo1-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32c3-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32c3cdc-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s2-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s2cdc-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s3-safeboot.bin
+curl -O https://github.com/arendst/Tasmota-firmware/raw/main/firmware/tasmota32/tasmota32s3cdc-safeboot.bin
