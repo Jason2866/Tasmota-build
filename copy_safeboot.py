20e55093059e863300faf104237a0c2957120b65
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-18 13:38:12 +0200

    Update copy_safeboot.py

--- copy_safeboot.py
+++ copy_safeboot.py
@@ -9,7 +9,7 @@ response = request.urlretrieve(URL, "tasmota32solo1-safeboot.bin")
 URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c3-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32c3-safeboot.bin")
 
-URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c3cdc-safeboot.bin"
+URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c3ser-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32c3ser-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32s2-safeboot.bin"
@@ -21,7 +21,7 @@ response = request.urlretrieve(URL, "tasmota32s2cdc-safeboot.bin")
 URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32s3-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32s3-safeboot.bin")
 
-URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32s3cdc-safeboot.bin"
+URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32s3ser-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32s3ser-safeboot.bin")
 
 URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c2-safeboot.bin"
@@ -30,5 +30,5 @@ response = request.urlretrieve(URL, "tasmota32c2-safeboot.bin")
 URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c6-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32c6-safeboot.bin")
 
-URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c6cdc-safeboot.bin"
+URL = "https://github.com/arendst/Tasmota-firmware/raw/firmware/firmware/tasmota32/tasmota32c6ser-safeboot.bin"
 response = request.urlretrieve(URL, "tasmota32c6ser-safeboot.bin")
