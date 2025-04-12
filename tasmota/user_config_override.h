b2c9796b1850677b92afd13c3b25156833964f7a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-12 15:50:57 +0200

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -780,8 +780,7 @@ ooooo     ooo ooooo      ooo oooooooooo.   oooooooooooo oooooooooooo ooooo ooooo
 #define USE_BERRY
 #undef USE_BERRY_TCPSERVER
 #define USE_WEBCLIENT
-//#define USE_WEBCLIENT_HTTPS
-#undef USE_WEBCLIENT_HTTPS
+#define USE_WEBCLIENT_HTTPS
 #define USE_MI_EXT_GUI
 #endif
 
